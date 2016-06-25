05199c090a1402fb7ee809b6371fc41f33ff4626
AgnieszkaStasiak <agnieszka.as.stasiak@gmail.com>
2016-06-25 13:05:09 +0200

    Stworzona strona ustawione grafiki

---
+++ ProjektSystemy/index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="content/css/styl.css" rel="stylesheet" type="text/css"/>
+        <link href="content/css/bootstrap-theme.css" rel="stylesheet" type="text/css"/>
+        <link href="content/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+        <link href="content/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="content/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="content/js/bootstrap.js" type="text/javascript"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title></title>
+    </head>
+    <body>
+        <div class="container-fluid col-lg-12">
+            <div class="panel-default panel-body">    
+                <div class="dom" id="dom">
+                <img src="content/img/domek projekt.png"  >
+            </div>
+            
+            
+          
+            <div class="grzejnikWl1" id="KaloryferWlaczony_1">
+                <img src="content/img/kaloryfer_wl.png" >
+            </div> 
+            <div  class="grzejnikWl2" id="KaloryferWlaczony_2">
+                <img src="content/img/kaloryfer_wl.png" >
+            </div>
+            
+            <div  class="grzejnikWl3" id="KaloryferWlaczony_3">
+                <img src="content/img/kaloryfer_wl.png" >
+
+            </div>
+            
+            <div  class="grzejnikWl4" id="KaloryferWlaczony_4">
+                <img src="content/img/kaloryfer_wl.png" >
+
+            </div>
+
+             <div  class="oknoOtw1" id="OknoPraweOtwarte">
+      
+                <img src="content/img/okno otworzne prawe konserwatywne.png" alt=""/>
+            </div>
+            
+             <div  class="oknoOtw2" id="OknoLeweOtwarte">
+                 <img src="content/img/lewackie okno otwarte na lewą stronę.png" alt=""/>
+
+            </div>
+            
+    
+
+          
+            
+             <div  class="OknoPodOtw" id="OknoDachoweOtwarte">
+                 <img src="content/img/dachowe okienko na świat otworzone.png" alt=""/>
+
+            </div>
+   
+            <div id="SwiatloWlaczone_1">
+                <img src="content/img/lampeczka świeci.png" alt=""/>
+                
+            </div>
+            
+             <div id="SwiatloWlaczone_2">
+                <img src="content/img/lampeczka świeci.png" alt=""/>
+                
+            </div>
+             <div id="SwiatloWlaczone_3">
+                <img src="content/img/lampeczka świeci.png" alt=""/>
+                
+            </div>
+             <div id="SwiatloWlaczone_4">
+                <img src="content/img/lampeczka świeci.png" alt=""/>
+                
+            </div>
+            
+            <div id="KlimatyzacjaWlaczona_1">
+                <img src="content/img/nibyklima dziala.png" alt=""/>
+            </div>
+            
+              
+            <div id="KlimatyzacjaWlaczona_2">
+                <img src="content/img/nibyklima dziala.png" alt=""/>
+            </div>
+                </div>
+            <div class="panel-default">
+                
+            </div>
+        </div>
+        <div class="panel-default" id="Ustawienia">
+            temperatura oczekiwana<br>
+            
+            <input id="temperatura" type="text"><br>
+            
+            temperatura na zewnatrz<br>
+            <br>
+            dzeszcz
+            <br>
+            wiatr
+            <br>
+            Wnętrze
+            <br>
+            temperatura<br>
+            okan elewacyjne<br>
+            okna dachowe<br>
+            ogrzewanie<br>
+            klimatyzacja<br>
+            
+            
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
